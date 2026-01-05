a9061134e522d75e1a39873489bdf1ef690780b2
Avinash <avinashmarbhal1994@gmail.com>
2026-01-05 23:29:45 +0530

    route changed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,7 +13,14 @@ export const routes: Routes = [
   { path: 'home', component: Home, title: `Home | ${Constants.APP_TITLE}` },
   { path: 'about', component: About, title: `About | ${Constants.APP_TITLE}` },
   { path: 'skills', component: Skills, title: `Skills | ${Constants.APP_TITLE}` },
-  { path: 'projects', component: UnderConstructionPage, title: `Projects | ${Constants.APP_TITLE}` },
   { path: 'contact', component: Contact, title: `Contact | ${Constants.APP_TITLE}` },
+  // { path: 'projects', component: Projects, title: `Projects | ${Constants.APP_TITLE}` },
+  { path: 'projects', component: UnderConstructionPage, title: `Projects | ${Constants.APP_TITLE}` },
+  { path: 'web-design', component: UnderConstructionPage, title: `Web Design | ${Constants.APP_TITLE}` },
+  { path: 'app-development', component: UnderConstructionPage, title: `App Development | ${Constants.APP_TITLE}` },
+  { path: 'ui/ux-design', component: UnderConstructionPage, title: `UI/UX Design | ${Constants.APP_TITLE}` },
+  { path: 'consulting', component: UnderConstructionPage, title: `Consulting | ${Constants.APP_TITLE}` },
+
+
   { path: '**', component: NotFound, title: `404 Not Found | ${Constants.APP_TITLE}` }
 ];
