9b63ce10f4ff20f4306077883c61a5c716a9c64c
Avinash <avinashmarbhal1994@gmail.com>
2026-01-07 00:10:08 +0530

    toastr added

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
+declare var $: any;
 
 @Component({
   selector: 'app-home',
@@ -12,4 +13,15 @@ export class Home {
   // Signals for dynamic data
   name = signal('Avinash Marbhal');
   role = signal('Front-End Developer');
+
+  downloadResume(): void {
+    const link = document.createElement('a');
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', 'assets/resume/Avinash-Marbhal-Resume-Angular-Updated.pdf'); // Path to your file
+    link.setAttribute('download', 'Avinash-Marbhal-Resume-Angular.pdf'); // Desired filename
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
+    $('#staticBackdrop').modal('show');
+  }
 }
